cfb8a6d34aea496c1de80cc85d88cba70364ab53
raihan <raihanp0308@gmail.com>
2023-06-01 14:10:53 +0700

    events

--- NodeJs Basic/events/index.js
+++ NodeJs Basic/events/index.js
@@ -9,9 +9,3 @@ const birthdayEventListener = (name) => {
 myEmitter.on('birthday', birthdayEventListener);
 
 myEmitter.emit('birthday', 'raihan');
-
-// TODO 3 : Tentukan birthdayEventListener sebagai aksi 
-// ketika event ‘birthday’ dibangkitkan pada myEmitter.
-// TODO 4 : Bangkitkanlah event ‘birthday’ pada myEmitter 
-// dengan method emit() dan beri nilai argumen listener 
-// dengan nama Anda.
