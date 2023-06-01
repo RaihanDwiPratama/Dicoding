56fb1f225acec059b6785dd4cf0e853e8203652c
raihan <raihanp0308@gmail.com>
2023-06-01 17:24:23 +0700

    response body

--- Nodejs-web-server/server.js
+++ Nodejs-web-server/server.js
@@ -13,12 +13,16 @@ const requestListener = (request, response) => {
         // jika metode get dipakai, maka akan menampilkan ini adalah homepage.
         if(method === 'GET') {
             response.statusCode = 200;
-            response.end("<h1>Ini adalah homepage</h1>");
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
         }
         // jika metode yang dipakai selain get, maka akan menampilkan output dibawah ini.
         else {
             response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
         }
     } 
     // url untuk about
@@ -26,7 +30,9 @@ const requestListener = (request, response) => {
         // jika metode get dipakai, maka akan menampilkan ini adalah halaman about
         if(method === 'GET') {
             response.statusCode = 200
-            response.end("<h1>Halo! Ini adalah halaman about</h1>");
+            response.end(JSON.stringify({
+                message: 'Halo! Ini adalah halaman about',
+            }));
         }
         // jika metode post dipakai, maka akan menampilkan data name pada body
         else if(method === 'POST') {
@@ -39,16 +45,22 @@ const requestListener = (request, response) => {
             request.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const {name} = JSON.parse(body);
-                response.end(`<h1>Halo! ${name}! Ini adalah halaman about</h1>`);
+                response.end(JSON.stringify({
+                    message: `Halo! ${name}! Ini adalah halaman about`,
+                }));
             });
         }
         else {
             response.statusCode = 400;
-            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`)
+            response.end(JSON.stringify({
+                message: `Halaman tidak dapat diakses dengan ${method} request`,
+            }));
         }
     } else {
         response.statusCode = 404;
-        response.end('<h1>Halaman tidak ditemukan</h1>');
+        response.end(JSON.stringify({
+            message: 'Halaman tidak ditemukan!',
+        }));
     }
 };
 
